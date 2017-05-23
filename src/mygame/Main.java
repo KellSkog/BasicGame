528025957ec28af202e59e8289852f7c46c4e727
KellSkog <kell.skog@gmail.com>
2017-05-23 15:29:45 +0200

    Ligth source added and walls are now visible.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -11,6 +11,7 @@ import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -57,13 +58,30 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
         addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, -3, 3), new Vector3f(1,-1,1));
 
         // The walls, does not move (mass=0)
-        Material wallMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Material wallMat = new Material(assetManager,  // Create new material and...
+            "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
+       
+//        Material wallMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+//        wallMat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/wood_diffuse.png")); // with Lighting.j3md
         addWall(wallMat, new Box(10.0f, 0.01f, 5.0f), new Vector3f(0, -5f, 0)); //Floor
         addWall(wallMat, new Box(10.0f, 0.01f, 5.0f), new Vector3f(0, 5f, 0)); //Ceiling
         addWall(wallMat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, 5f)); //Long wall
         addWall(wallMat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, -5f)); //Long wall
         addWall(wallMat, new Box(0.01f, 5.0f, 5.0f), new Vector3f(-10.0f, 0.0f, 0.0f)); //Gable
         addWall(wallMat, new Box(0.01f, 5.0f, 5.0f), new Vector3f(10.0f, 0.0f, 0.0f)); //Gable
+        
+        addLight();
+    }
+    public void addLight() {
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+//        PointLight lamp_light = new PointLight();
+//        lamp_light.setColor(ColorRGBA.White);
+//        lamp_light.setRadius(4f);
+//        lamp_light.setPosition(new Vector3f(-5f, -5f, 5f));
+//        lamp_light.setPosition(new Vector3f(lamp_geo.getLocalTranslation()));
+        rootNode.addLight(sun);
     }
     public void addBall(Material material, Mesh ball, float mass, Vector3f localTranslation, Vector3f initialVelocity) {
             Geometry geom = new Geometry("Box", ball);
@@ -77,6 +95,7 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
              //Set initial test velocity
             bulletControl.setLinearVelocity(initialVelocity);
             bulletControl.setRestitution(1);
+//            bulletControl.setFriction(50f); //Has no effect!
             rootNode.attachChild(geom);
             // activate physics
             PhysicsSpace space = bulletAppState.getPhysicsSpace();
@@ -86,8 +105,12 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
             space.addCollisionListener(this);
     }
     public void addWall(Material material, Box floorBox, Vector3f localTranslation) {
-        material.setColor("Color", ColorRGBA.Yellow);
-        material.getAdditionalRenderState().setWireframe(true);
+        material.setBoolean("UseMaterialColors",true);  // Set some parameters, e.g. blue.
+        material.setColor("Ambient", ColorRGBA.Green);   // ... color of this object
+        material.setColor("Diffuse", ColorRGBA.Green);   // ... color of light being reflected
+        
+//        material.setColor("Color", ColorRGBA.Yellow);
+//        material.getAdditionalRenderState().setWireframe(true);
         MeshCollisionShape meshCollisionShape = new MeshCollisionShape(floorBox);
         RigidBodyControl rigidBodyControl = new RigidBodyControl(meshCollisionShape, 0f);
         rigidBodyControl.setKinematic(false);

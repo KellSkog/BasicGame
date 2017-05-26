75f28dff49c8947f5f23e0e441e6256236a94e70
KellSkog <kell.skog@gmail.com>
2017-05-26 14:10:56 +0200

    Transparancy has  been achieved, and it is amazing :-)

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -51,55 +51,36 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
         Material  ballMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, 3, -3), new Vector3f(-1,1f,0));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, 3, -3), new Vector3f(1,1,0));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, -3, -3), new Vector3f(-1,-1,0));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, -3, -3), new Vector3f(1,-1,0));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, 3, 3), new Vector3f(-1,1f,1));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, 3, 3), new Vector3f(1,1,1));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, -3, 3), new Vector3f(-1,-1,1));
-        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, -3, 3), new Vector3f(1,-1,1));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, 3, -3), new Vector3f(-2,2f,0));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, 3, -3), new Vector3f(2,2,0));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, -3, -3), new Vector3f(-2,-2,0));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, -3, -3), new Vector3f(2,-2,0));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, 3, 3), new Vector3f(-2,2,2));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, 3, 3), new Vector3f(2,2,2));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(-3, -3, 3), new Vector3f(-2,-2,2));
+        addBall(ballMat, new Sphere(30, 30, radius), mass, new Vector3f(3, -3, 3), new Vector3f(2,-2,2));
 
         // The walls, does not move (mass=0)
-        Material wallMat = new Material(assetManager,  // Create new material and...
-            "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
-       
-//        Material wallMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+//        Material wallMat = new Material(assetManager,  // Create new material and...
+//            "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
 //        wallMat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/wood_diffuse.png")); // with Lighting.j3md
+//        wallMat.setBoolean("UseMaterialColors",true);  // Only for Lighting
+//        wallMat.setColor("Ambient", ColorRGBA.Green);   // Only for Lighting
+//        wallMat.setColor("Diffuse", ColorRGBA.Green);   // Only for Lighting        
+
+        Material wallMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        wallMat.setColor("Color", ColorRGBA.Green);   // Color of Unshaded
+        wallMat.setTexture("ColorMap", assetManager.loadTexture("felt_green.jpg"));// In project 'assets' directory
         addWall(wallMat, new Box(10.0f, 0.01f, 5.0f), new Vector3f(0, -5f, 0), WallProperty.VISIBLE); //Floor
         addWall(wallMat, new Box(10.0f, 0.01f, 5.0f), new Vector3f(0, 5f, 0), WallProperty.VISIBLE); //Ceiling
-        addWall(wallMat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, 5f), WallProperty.VISIBLE); //Long wall
-//        addWall(wallMat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, -5f), WallProperty.INVISIBLE); //Long wall
+        addWall(wallMat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, -5f), WallProperty.VISIBLE); //Long wall
         addWall(wallMat, new Box(0.01f, 5.0f, 5.0f), new Vector3f(-10.0f, 0.0f, 0.0f), WallProperty.VISIBLE); //Gable
         addWall(wallMat, new Box(0.01f, 5.0f, 5.0f), new Vector3f(10.0f, 0.0f, 0.0f), WallProperty.VISIBLE); //Gable
         
-//        Material mat = assetManager.loadMaterial("Textures/Terrain/Pond/Pond.j3m");
-//        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-        mat.setColor("Ambient", ColorRGBA.DarkGray);
-        mat.setColor("Diffuse", ColorRGBA.White);
-//        mat.setBoolean("UseMaterialColors", true);
-//        mat.setColor("Color", ColorRGBA.Green); // with Unshaded.j3md
-        
-        
-//        Material transWall = new Material(assetManager,  // Create new material and...
-////            "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
-//             "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
-//        //Configuring a colored material:
-////        transWall.setColor("Color", ColorRGBA.Green);// IllegalArgumentException: Material parameter is not defined: Color
-//        System.out.println("And  now follws the parameters:");
-//        transWall.getParams().stream()
-//            .forEach(System.out::println);
-//        System.out.println("And thats all folks, now follws an error!");
-//        transWall.setColor("Green", new ColorRGBA(0,1,0,0.5f));
-////        transWall.setTexture("AlphaMap", assetManager.loadTexture("Textures/window_alpha.png"));
-//          transWall.setBoolean("UseMaterialColors",true); // with Lighting.j3md
-////          transWall.setColor("Ambient", ColorRGBA.Green);   // ... color of this object
-////          transWall.setColor("Diffuse", ColorRGBA.Green);   // ... color of light being reflected
-////        transWall.setBoolean("UseAlpha",true);
-////        transWall.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
-        
-        addWall(mat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, -5f), WallProperty.INVISIBLE); //Long wall
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", new ColorRGBA(0f, 1f, 0f, 0f));   // Color of Unshaded
+//        mat.setTexture("ColorMap", assetManager.loadTexture("felt_green.jpg"));// In project 'assets' directory
+        addWall(mat, new Box(10.0f, 5.0f, 0.01f), new Vector3f(0, 0f, 5f), WallProperty.INVISIBLE); //Long wall
         
         addLight();
     }
@@ -118,13 +99,13 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
             Geometry geom = new Geometry("Box", ball);
             geom.setLocalTranslation(localTranslation);
             geom.addControl(new RigidBodyControl(.001f));
-            material.setColor("Color", ColorRGBA.Green);
+            material.setColor("Color", ColorRGBA.Blue);
             geom.setMaterial(material);
             RigidBodyControl bulletControl = new RigidBodyControl(mass);
             geom.addControl(bulletControl);
             bulletControl.setSleepingThresholds(0f,0f);
-             //Set initial test velocity
-            bulletControl.setLinearVelocity(initialVelocity);
+             
+            bulletControl.setLinearVelocity(initialVelocity); //Set initial test velocity
             bulletControl.setRestitution(1);
 //            bulletControl.setFriction(50f); //Has no effect!
             rootNode.attachChild(geom);
@@ -136,12 +117,6 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
             space.addCollisionListener(this);
     }
     public void addWall(Material material, Box floorBox, Vector3f localTranslation, WallProperty visibility) {
-        material.setBoolean("UseMaterialColors",true);  // Set some parameters, e.g. blue.
-        material.setColor("Ambient", ColorRGBA.Green);   // ... color of this object
-        material.setColor("Diffuse", ColorRGBA.Green);   // ... color of light being reflected
-        
-//        material.setColor("Color", ColorRGBA.Yellow);
-//        material.getAdditionalRenderState().setWireframe(true);
         MeshCollisionShape meshCollisionShape = new MeshCollisionShape(floorBox);
         RigidBodyControl rigidBodyControl = new RigidBodyControl(meshCollisionShape, 0f);
         rigidBodyControl.setKinematic(false);
@@ -149,8 +124,10 @@ public class Main extends SimpleApplication implements PhysicsCollisionListener 
 //        floorBox.scaleTextureCoordinates(new Vector2f(3, 6));
         
         Geometry floor = new Geometry("floor", floorBox);
-        if (visibility == WallProperty.INVISIBLE)
+        if (visibility == WallProperty.INVISIBLE) {
+            material.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
             floor.setQueueBucket(Bucket.Transparent);
+        }
         floor.setMaterial(material);
         floor.setLocalTranslation(localTranslation);
         floor.addControl(rigidBodyControl);
